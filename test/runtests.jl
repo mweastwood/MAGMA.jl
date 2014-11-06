45582a5af7a04b954ef7d5d39aec12155193bd35
Michael Eastwood <mweastwood@astro.caltech.edu>
2014-11-06 13:11:40 -0800

    more thorough testing (dot, axpy!, gemv!)

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,36 @@ include("../src/MAGMA.jl")
 using MAGMA
 using Base.Test
 
-let x = [1.:100.], y = [101.:200.]
+N = 100
+srand(123)
+
+# test dot
+let x = randn(N), y = randn(N)
     dx = MagmaVector(x)
     dy = MagmaVector(y)
     @test x == get(dx)
     @test y == get(dy)
-    @test dot(x,y) == dot(dx,dy)
+    @test_approx_eq dot(x,y) dot(dx,dy)
+end
+
+# test axpy!
+let a = randn(), x = randn(N), y = randn(N)
+    dx = MagmaVector(x)
+    dy = MagmaVector(y)
+    @test x == get(dx)
+    @test y == get(dy)
+    axpy!(a,dx,dy)
+    @test_approx_eq a*x+y get(dy)
+end
+
+# test gemv!
+let a = randn(), b = randn(), A = randn(N,N), x = randn(N), y = randn(N)
+    dA = MagmaMatrix(A)
+    dx = MagmaVector(x)
+    dy = MagmaVector(y)
+    @test x == get(dx)
+    @test y == get(dy)
+    gemv!(a,dA,dx,b,dy)
+    @test_approx_eq a*A*x+b*y get(dy)
 end
 
